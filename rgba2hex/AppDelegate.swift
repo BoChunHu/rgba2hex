1fc7f1c02a3d7d742aee7030c4ba9787dc93756f
James Hu <james.hu@fable.com.tw>
2016-01-05 12:06:31 +0800

    Initial Commit

---
+++ rgba2hex/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  rgba2hex
+//
+//  Created by Mac on 2016/1/5.
+//  Copyright © 2016年 James. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
